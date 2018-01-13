a18985dc9c0abaf545dd5e8a09f59a33dc139d93
GreatLove <q13426591632@126.com>
2018-01-13 19:35:37 +0800

    去除了训练数据ngram中的标点

--- Ngrams/generate_feature.py
+++ Ngrams/generate_feature.py
@@ -1,6 +1,12 @@
 #coding:utf-8
 
 def main():
+    f = open('punc.txt', 'r')
+    punc = {}
+    for line in f:
+        punc[line.strip()] = 0
+    f.close()
+
     featureDir = 'Processed/feature/'
     labelDir = 'Processed/label/'
     outDir = 'feature/'
@@ -8,7 +14,7 @@ def main():
         '双十一购物节', '德国大选', '功守道', '战狼2', '权力的游戏', '李晨求婚范冰冰', '江歌刘鑫',
         '王宝强马蓉离婚案', '百度无人驾驶汽车', '红黄蓝幼儿园', '绝地求生 吃鸡', '英国脱欧',
         '萨德系统 中韩', '雄安新区', '榆林产妇坠楼']
-    for i in range(10, 20):
+    for i in range(0, 20):
         newsName = news[i]
         ff = open(unicode(featureDir+newsName+'.txt', 'utf8'), 'r')
         fl = open(unicode(labelDir+newsName+'.txt', 'utf8'), 'r')
@@ -51,6 +57,9 @@ def main():
 
         #输出到feature/中
         for k in range(0, l):
+            tmp_ngram = ngram[k].split('+')
+            if len(tmp_ngram) == 3 and tmp_ngram[1] in punc:
+                continue
             fw.write(ngram[k]+' '+score[k])
             for j in range(0, 7):
                 fw.write(' '+str(features[k][j]))
