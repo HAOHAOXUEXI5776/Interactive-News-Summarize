a4e11cbc2cff1758555acf306a5637506e2e3f15
GreatLove <q13426591632@126.com>
2018-01-20 21:55:50 +0800

    带mmr的pagerank与块排序

---
+++ RankSort/sort.py
@@ -0,0 +1,204 @@
+#coding: utf-8
+
+import gensim
+from pyltp import Segmentor
+import os
+from math import *
+
+qin = 1 #改成0是刘辉的路径，否则是秦文涛的路径
+# 加载分词模型
+segmentor = Segmentor()
+if qin == 1:
+    segmentor.load('D:/coding/Python2.7/ltp_data_v3.4.0/cws.model')
+    model = gensim.models.Word2Vec.load('../Sentence/model_qin')
+else:
+    segmentor.load('/Users/liuhui/Desktop/实验室/LTP/ltp_data_v3.4.0/cws.model')
+    model = gensim.models.Word2Vec.load('../Sentence/model')
+vec_size = 100
+
+stoplist = {}
+f = open('../stopword.txt', 'r')
+for line in f:
+    word = line.strip()
+    stoplist[word] = 1
+f.close()
+
+class Sent:
+    def __init__(self, _newsid, _globalid, _paraid, _localid, _sentnum, _content, _vec):
+        self.newsid = _newsid        #该句所属新闻编号
+        self.globalid = _globalid    #该句在该篇新闻的第几句
+        self.paraid = _paraid        #该句在该篇新闻的第几段
+        self.localid = _localid      #该句在所属段的第几句
+        self.sentnum = _sentnum       #该句所在段有多少句
+        self.content = _content
+        self.vec = _vec
+
+# 计算几个向量的平均向量
+def mean_vec(vec_list):
+    if len(vec_list) == 0:
+        return [0.0 for k in range(0, vec_size)]
+    result = [0.0 for k in range(0, len(vec_list[0]))]
+    for vec in vec_list:
+        for k in range(0, len(result)):
+            result[k] += vec[k]
+    for k in range(0, len(result)):
+        result[k] /= len(vec_list)
+    return result
+
+# 计算两个向量的余弦相似度
+def cos_similarity(vec1, vec2):
+    result = 0.0
+    vec1_size = 0.0
+    vec2_size = 0.0
+    for k in range(0, len(vec1)):
+        result += vec1[k] * vec2[k]
+        vec1_size += vec1[k] * vec1[k]
+        vec2_size += vec2[k] * vec2[k]
+    if vec1_size < 1e-10 or vec2_size < 1e-10:
+        return 0.0
+    result /= sqrt(vec1_size * vec2_size)
+    return result
+
+#根据块的前后关系behind得到整体的排序
+#首先根据blocks[0]，将其余的blocks划分到它的两侧，对两侧递归处理
+#排序结果置于li中
+def recursort(behind, li, i, index, offset):
+    if len(index) == 0:
+        return
+    if len(index) == 1:
+        li[i] = offset
+        return
+    r = [] #在i右边的
+    l = [] #在i左边的
+    for k in index:
+        if k == i:
+            continue
+        if behind[i][k] == 1:
+            l.append(k)
+        elif behind[i][k] == 0:
+            r.append(k)
+    li[i] = offset + len(l) #i放在第len(l)的位置
+    if len(r) > 0:
+        recursort(behind, li, r[0], r, li[i]+1)
+    if len(l) > 0:
+        recursort(behind, li, l[0], l, offset)
+
+
+news_name = ['hpv疫苗', 'iPhone X', '乌镇互联网大会', '九寨沟7.0级地震', '俄罗斯世界杯',
+             '双十一购物节', '德国大选', '功守道', '战狼2', '权力的游戏', '李晨求婚范冰冰', '江歌刘鑫',
+             '王宝强马蓉离婚案', '百度无人驾驶汽车', '红黄蓝幼儿园', '绝地求生 吃鸡', '英国脱欧',
+             '萨德系统 中韩', '雄安新区', '榆林产妇坠楼']
+
+outDir = 'summary/'
+for news in news_name:
+    print news
+    path = outDir+news
+    if not os.path.exists(unicode(path, 'utf8')):
+        os.mkdir(unicode(path, 'utf8'))
+
+    #读取所有的新闻
+    new = []   #记录所有新闻对应的句子
+    for i in range(0, 100):
+        #默认最多100篇新闻
+        t = []
+        new.append(t)
+    f = open(unicode('../Sentence/sentence/'+news+'/sentence.txt','utf8'), 'r')
+    while True:
+        nums = f.readline()
+        if len(nums) == 0:
+            break
+        content = f.readline().strip()
+        nums = nums.strip().split()
+        nums = [int(num) for num in nums]
+        words = segmentor.segment(content)
+        word_vec_list = []
+        for word in words:
+            if word not in stoplist and word in model:
+                word_vec_list.append(model[word])
+        sent = Sent(nums[0],nums[1],nums[2],nums[3],nums[4],content,mean_vec(word_vec_list))
+        new[nums[0]-1].append(sent)
+    f.close()
+
+    #读入所有标签
+    f = open(unicode('../Sentence/label/'+news+'/label.txt', 'utf8'), 'r')
+    labels = [line.strip().replace('+', '') for line in f]
+    f.close()
+
+    for label in labels:
+        print label
+        #读取排序后的块
+        blocks = [] #记录该标签对应的块
+        f = open(unicode('blockscore/'+news+'/'+label+'.txt', 'utf8'), 'r')
+        while True:
+            n = f.readline() #第一行记录了下面几个句子是一个块
+            if len(n) == 0:
+                break
+            block = []
+            n = int(n.strip())
+            for i in range(0, n):
+                nums = f.readline()
+                nums = nums.strip().split()
+                nums = [int(num) for num in nums]
+                content = f.readline().strip()
+                words = segmentor.segment(content)
+                word_vec_list = []
+                for word in words:
+                    if word not in stoplist and word in model:
+                        word_vec_list.append(model[word])
+                sent = Sent(nums[0],nums[1],nums[2],nums[3],nums[4],content,mean_vec(word_vec_list))
+                block.append(sent)
+            blocks.append(block)
+        f.close()
+        blockcnt = len(blocks)
+
+        #选择blocks中的若干块，使得其总句子数在大于10的情况下尽可能小
+        tolsent, endblock = 0, 0
+        while tolsent < 10 and endblock < blockcnt:
+            tolsent += len(blocks[endblock])
+            endblock += 1
+        assert endblock != 0
+
+        #对0~endblock-1的块进行排序
+        #对每个块，计算其他的块是放在它的前头还是后头
+        #behind[i][j] = 1（或behind[j][i] = 0）(i!=j)，则说明i应放在j之后
+        behind = []
+        for i in range(0, endblock):
+            tmp = [-1 for i in range(0, endblock)]
+            behind.append(tmp)
+        for i in range(0, endblock):
+            for j in range(i+1, endblock):
+                newi = blocks[i][0].newsid-1 #块i所在的新闻编号
+                newj = blocks[j][0].newsid-1 #块j所在的新闻编号
+
+                # 计算块j与块i在新闻newi中前半部分和后半部分的相似度，若与前半部分较为相似，则j放i前头，反之，放后头
+                newi_front, newi_behind = [], []
+                blocki_start = blocks[i][0].globalid
+                blocki_end = blocks[i][len(blocks[i])-1].globalid
+                for sent in new[newi]:
+                    if sent.globalid < blocki_start:
+                        newi_front.append(sent)
+                    elif sent.globalid > blocki_end:
+                        newi_behind.append(sent)
+                simi_front, simi_behind = 0.0, 0.0
+                for sent in newi_front:
+                    for sentj in blocks[j]:
+                        simi_front = max(simi_front, cos_similarity(sent.vec, sentj.vec))
+                for sent in newi_behind:
+                    for sentj in blocks[j]:
+                        simi_behind = max(simi_behind, cos_similarity(sent.vec, sentj.vec))
+                if simi_behind > simi_front:
+                    behind[i][j], behind[j][i] = 0, 1
+                else:
+                    behind[i][j], behind[j][i] = 1, 0
+
+        sort_index = [-1 for i in range(0, endblock)]
+        index = [i for i in range(0, endblock)]
+        recursort(behind, sort_index, 0, index, 0)
+
+        f = open(unicode(outDir+news+'/'+label+'.txt', 'utf8'), 'w')
+        for sinx in sort_index:
+            for sent in blocks[sinx]:
+                f.write(sent.content+'\n')
+            f.write('\n')
+        f.close()
+
